7a5d14f5e0b07dbd08d651985d8affe2ab7f088f
Nicolas Soo <scarecrow2003@gmail.com>
2015-05-05 22:32:55 +0800

    login redirect

--- override/controllers/front/AuthController.php
+++ override/controllers/front/AuthController.php
@@ -61,6 +61,103 @@ class AuthController extends AuthControllerCore
     }
 
     /**
+     * Process login
+     */
+    protected function processSubmitLogin()
+    {
+        Hook::exec('actionBeforeAuthentication');
+        $passwd = trim(Tools::getValue('passwd'));
+        $email = trim(Tools::getValue('email'));
+        if (empty($email))
+            $this->errors[] = Tools::displayError('An email address required.');
+        elseif (!Validate::isEmail($email))
+            $this->errors[] = Tools::displayError('Invalid email address.');
+        elseif (empty($passwd))
+            $this->errors[] = Tools::displayError('Password is required.');
+        elseif (!Validate::isPasswd($passwd))
+            $this->errors[] = Tools::displayError('Invalid password.');
+        else
+        {
+            $customer = new Customer();
+            $authentication = $customer->getByEmail(trim($email), trim($passwd));
+            if (isset($authentication->active) && !$authentication->active)
+                $this->errors[] = Tools::displayError('Your account isn\'t available at this time, please contact us');
+            elseif (!$authentication || !$customer->id)
+                $this->errors[] = Tools::displayError('Authentication failed.');
+            else
+            {
+                $this->context->cookie->id_compare = isset($this->context->cookie->id_compare) ? $this->context->cookie->id_compare: CompareProduct::getIdCompareByIdCustomer($customer->id);
+                $this->context->cookie->id_customer = (int)($customer->id);
+                $this->context->cookie->customer_lastname = $customer->lastname;
+                $this->context->cookie->customer_firstname = $customer->firstname;
+                $this->context->cookie->logged = 1;
+                $customer->logged = 1;
+                $this->context->cookie->is_guest = $customer->isGuest();
+                $this->context->cookie->passwd = $customer->passwd;
+                $this->context->cookie->email = $customer->email;
+
+                // Add customer to the context
+                $this->context->customer = $customer;
+
+                if (Configuration::get('PS_CART_FOLLOWING') && (empty($this->context->cookie->id_cart) || Cart::getNbProducts($this->context->cookie->id_cart) == 0) && $id_cart = (int)Cart::lastNoneOrderedCart($this->context->customer->id))
+                    $this->context->cart = new Cart($id_cart);
+                else
+                {
+                    $id_carrier = (int)$this->context->cart->id_carrier;
+                    $this->context->cart->id_carrier = 0;
+                    $this->context->cart->setDeliveryOption(null);
+                    $this->context->cart->id_address_delivery = (int)Address::getFirstCustomerAddressId((int)($customer->id));
+                    $this->context->cart->id_address_invoice = (int)Address::getFirstCustomerAddressId((int)($customer->id));
+                }
+                $this->context->cart->id_customer = (int)$customer->id;
+                $this->context->cart->secure_key = $customer->secure_key;
+
+                if ($this->ajax && isset($id_carrier) && $id_carrier && Configuration::get('PS_ORDER_PROCESS_TYPE'))
+                {
+                    $delivery_option = array($this->context->cart->id_address_delivery => $id_carrier.',');
+                    $this->context->cart->setDeliveryOption($delivery_option);
+                }
+
+                $this->context->cart->save();
+                $this->context->cookie->id_cart = (int)$this->context->cart->id;
+                $this->context->cookie->write();
+                $this->context->cart->autosetProductAddress();
+
+                Hook::exec('actionAuthentication');
+
+                // Login information have changed, so we check if the cart rules still apply
+                CartRule::autoRemoveFromCart($this->context);
+                CartRule::autoAddToCart($this->context);
+
+                if (!$this->ajax && $back = Tools::getValue('back'))
+                {
+                    if ($customer->nickname == null) {
+                        Tools::redirect('index.php?controller=identity');
+                    }
+
+                    if ($back == Tools::secureReferrer(Tools::getValue('back')))
+                        Tools::redirect(html_entity_decode($back));
+
+                    $back = $back ? $back : 'my-account';
+                    Tools::redirect('index.php?controller='.(($this->authRedirection !== false) ? urlencode($this->authRedirection) : $back));
+                }
+            }
+        }
+        if ($this->ajax)
+        {
+            $return = array(
+                'hasError' => !empty($this->errors),
+                'errors' => $this->errors,
+                'token' => Tools::getToken(false)
+            );
+            $this->ajaxDie(Tools::jsonEncode($return));
+        }
+        else
+            $this->context->smarty->assign('authentification_error', $this->errors);
+
+    }
+
+    /**
      * Process submit on an account
      */
     protected function processSubmitAccount()
