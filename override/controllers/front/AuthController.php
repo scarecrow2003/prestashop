f67496f1e9ea86fef31dec2894d21cf0a49a3e54
Nicolas Soo <scarecrow2003@gmail.com>
2015-05-06 22:58:25 +0800

    move auth function to override

--- override/controllers/front/AuthController.php
+++ override/controllers/front/AuthController.php
@@ -9,6 +9,94 @@
 class AuthController extends AuthControllerCore
 {
 
+    /**
+     * Assign template vars related to page content
+     * @see FrontController::initContent()
+     */
+    public function initContent()
+    {
+        parent::initContent();
+
+        $this->context->smarty->assign('genders', Gender::getGenders());
+
+        $this->assignDate();
+
+        $this->assignCountries();
+
+        $newsletter = Configuration::get('PS_CUSTOMER_NWSL') || (Module::isInstalled('blocknewsletter') && Module::getInstanceByName('blocknewsletter')->active);
+        $this->context->smarty->assign('newsletter', $newsletter);
+        $this->context->smarty->assign('optin', (bool)Configuration::get('PS_CUSTOMER_OPTIN'));
+
+        $back = Tools::getValue('back');
+        $key = Tools::safeOutput(Tools::getValue('key'));
+
+        if (!empty($key))
+            $back .= (strpos($back, '?') !== false ? '&' : '?').'key='.$key;
+
+        if ($back == Tools::secureReferrer(Tools::getValue('back')))
+            $this->context->smarty->assign('back', html_entity_decode($back));
+        else
+            $this->context->smarty->assign('back', Tools::safeOutput($back));
+
+        if (Tools::getValue('display_guest_checkout'))
+        {
+            if (Configuration::get('PS_RESTRICT_DELIVERED_COUNTRIES'))
+                $countries = Carrier::getDeliveredCountries($this->context->language->id, true, true);
+            else
+                $countries = Country::getCountries($this->context->language->id, true);
+
+            $this->context->smarty->assign(array(
+                'inOrderProcess' => true,
+                'PS_GUEST_CHECKOUT_ENABLED' => Configuration::get('PS_GUEST_CHECKOUT_ENABLED'),
+                'PS_REGISTRATION_PROCESS_TYPE' => Configuration::get('PS_REGISTRATION_PROCESS_TYPE'),
+                'sl_country' => (int)$this->id_country,
+                'countries' => $countries
+            ));
+        }
+
+        if (Tools::getValue('create_account'))
+            $this->context->smarty->assign('email_create', 1);
+
+        if (Tools::getValue('multi-shipping') == 1)
+            $this->context->smarty->assign('multi_shipping', true);
+        else
+            $this->context->smarty->assign('multi_shipping', false);
+
+        $this->context->smarty->assign('field_required', $this->context->customer->validateFieldsRequiredDatabase());
+
+        $this->assignAddressFormat();
+
+        // Call a hook to display more information on form
+        $this->context->smarty->assign(array(
+            'HOOK_CREATE_ACCOUNT_FORM' => Hook::exec('displayCustomerAccountForm'),
+            'HOOK_CREATE_ACCOUNT_TOP' => Hook::exec('displayCustomerAccountFormTop')
+        ));
+
+        $this->context->smarty->assign(array(
+            'LOGIN_HOOK' => Hook::exec('loginHook')
+        ));
+
+        // Just set $this->template value here in case it's used by Ajax
+        $this->setTemplate(_PS_THEME_DIR_.'authentication.tpl');
+
+        if ($this->ajax)
+        {
+            // Call a hook to display more information on form
+            $this->context->smarty->assign(array(
+                'PS_REGISTRATION_PROCESS_TYPE' => Configuration::get('PS_REGISTRATION_PROCESS_TYPE'),
+                'genders' => Gender::getGenders()
+            ));
+
+            $return = array(
+                'hasError' => !empty($this->errors),
+                'errors' => $this->errors,
+                'page' => $this->context->smarty->fetch($this->template),
+                'token' => Tools::getToken(false)
+            );
+            $this->ajaxDie(Tools::jsonEncode($return));
+        }
+    }
+
     public function postProcess()
     {
         if (Tools::isSubmit('SubmitCreate'))
